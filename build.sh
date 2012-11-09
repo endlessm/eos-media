6e49ccf3be99894c460d1f7789c9799e48ec2331
erik.wolfe <erik.wolfe@asolutions.com>
2012-11-09 14:24:40 -0600

    Adds debian build script and deb config files. Erik

---
+++ build.sh
@@ -0,0 +1,25 @@
+#!/bin/bash -e
+
+# Install dependencies
+sudo apt-get install -y devscripts debhelper
+
+pushd `dirname $0`
+  # Clean up old artifacts
+   set +e
+		rm -rf *.deb
+		rm -rf *.changes
+   set -e
+
+	pushd ../eos-installer &> /dev/null
+      INSTALL_DIR=$(pwd)
+		export GNUPGHOME=${INSTALL_DIR}/gnupg
+	popd &> /dev/null
+
+  # Build package
+  debuild -k4EB55A92 -b
+  
+  # Move package to this directory and clean up
+   mv ../*os-media*.deb .
+   mv ../*os-media*.changes .
+   rm ../*.build
+popd
