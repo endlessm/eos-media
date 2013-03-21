b090395d8f34e356c05cf53f9771c8fb9ca679a7
Matt Todd <matt.c.todd@gmail.com>
2013-03-21 08:33:50 -0500

    improved build script -Srdjan & Matt T

--- build.sh
+++ build.sh
@@ -1,7 +1,15 @@
 #!/bin/bash -e
 
 # Install dependencies
-sudo apt-get install -y devscripts debhelper
+DEPENDENCIES="devscripts debhelper"
+set +e 
+  dpkg -s $DEPENDENCIES &> /dev/null
+  has_dependencies=$?
+set -e
+
+if [[ $has_dependencies -ne 0 ]]; then
+  sudo apt-get install -y $DEPENDENCIES
+fi
 
 pushd `dirname $0`
   # Clean up old artifacts
